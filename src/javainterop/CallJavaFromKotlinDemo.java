db1a1f4dc422dfa5e9e3db9b25aa69d716d16003
yueban <fbzhh007@gmail.com>
2018-11-30 18:11:33 +0800

    add CallJavaFromKotlin demo

---
+++ src/javainterop/CallJavaFromKotlinDemo.java
@@ -0,0 +1,32 @@
+package javainterop;
+
+import java.util.List;
+
+public class CallJavaFromKotlinDemo {
+    /********** demo 6 **********/
+    public Foo<? extends Bar> a;
+    public Foo<? super Bar> b;
+    public List c;
+
+    /********** demo 3 **********/
+    public void is() {
+        System.out.println("is method");
+    }
+
+    /********** demo 7 **********/
+    public void removeIndices(int[] indices) {
+    }
+
+    /********** demo 8 **********/
+    public void removeIndicesVarArg(int... indices) {
+    }
+}
+
+class Foo<T> {
+}
+
+class Bar {
+}
+
+
+
