e412dcec36766fd509beed20d5a70c1d8c4c80ca
yueban <fbzhh007@gmail.com>
2019-09-14 21:28:44 +0800

    add kaixue.io demo: generic.kt

---
+++ src/main/kotlin/kaixue/basic/generic.kt
@@ -0,0 +1,25 @@
+package kaixue.basic
+
+fun main() {
+    /*val arr1 = arrayOfNulls<CharSequence>(1)
+    println(arr1.contentToString())
+    fill(arr1, "123123")
+    println(arr1.contentToString())*/
+
+
+    val arr2: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+    val arr3 = arrayOfNulls<Number>(5)
+    println(arr3.contentToString())
+    copy(arr2, arr3)
+    println(arr3.contentToString())
+}
+
+fun <T> fill(arr: Array<in T>, t: T) {
+    arr[0] = t
+}
+
+fun <T> copy(source: Array<out T>, dest: Array<in T>) {
+    for (item in source.withIndex()) {
+        dest[item.index] = item.value
+    }
+}
